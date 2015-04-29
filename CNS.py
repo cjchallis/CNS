56b319d45a2082d44c982c624e876bd00463823f
cjchallis <christopher.challis@gmail.com>
2015-04-29 16:57:51 -0400

    Print frequencies, first data corrections

--- CNS.py
+++ CNS.py
@@ -2,6 +2,15 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import chi2_contingency
+from collections import Counter
 
 cns_df = pandas.io.excel.read_excel("CNSdata.xlsx", "Sheet1")
-print(cns_df.pivot_table(rows = "Head", cols = "Arms"))
+columns = list(cns_df.columns.values)
+for i in range(3, len(columns) - 1):
+    print(columns[i])
+    print(Counter(cns_df[columns[i]]))
+'''    
+for i in range(3, len(columns) - 1):
+    for j in range(i+1, len(columns) - 1):
+        print(pandas.crosstab(cns_df[columns[i]], cns_df[columns[j]]))
+'''
